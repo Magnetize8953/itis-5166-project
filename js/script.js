f53a8267260ec703f3c1e4f874afc574c2038c54
Pineapple Farmer <dlopez18@uncc.edu>
2024-08-31 16:59:36 -0400

    feat: Updating header and footer using script.js
    Added new Logo file to span the header better! :D

--- js/script.js
+++ js/script.js
@@ -1,20 +1,34 @@
-// Make header the same on all pages
+// Make the header the same on all pages
 header = document.createElement("header");
-header.innerHTML = "<h1>Header</h1>";
-// Make the navigation bar the same on all pages
-navigation = document.createElement("nav");
-navigation.innerHTML = "<a href='index.html'>Home</a> <a href='events.html'>Events</a> <a href='about.html'>About</a>";
-// Make the content area the same on all pages
-content = document.createElement("div");
-content.innerHTML = "<h1>Content</h1>";
+header.innerHTML = `
+<div class="header-content">
+  <img src="../images/ColorStack_UNC-Charlotte_Logo.png" alt="Logo" id="logo">
+  <nav id="user-navigation">
+    <a id="login" href="login.html">Login</a>
+    <a id="signup" href="signup.html">Signup</a>
+  </nav>
+</div>`;
+
+
+// Make the main navigation bar the same on all pages
+mainNavigation = document.createElement("nav");
+mainNavigation.id = "main-navigation";
+mainNavigation.innerHTML = `
+<a href="index.html">Home</a>
+<a href="events.html">Events</a>
+<a href="newEvent.html">Create Event</a>`;
+// Add main navigation to the header
+header.appendChild(mainNavigation);
+
 // Make the footer the same on all pages
 footer = document.createElement("footer");
-footer.innerHTML = "<p>Footer</p>";
+footer.innerHTML = `
+<p>&copy; 2024 ColorStack at UNC Charlotte. All rights reserved.</p>
+<a href="about.html">ABOUT</a>
+<a href="contact.html">CONTACT</a>`;
 
-// Append the header, navigation, content, and footer to the body
-document.body.appendChild(header);
-document.body.appendChild(navigation);
-document.body.appendChild(content);
+// Append the header to the top of the body
+document.body.insertBefore(header, document.body.firstChild);
+
+// Append the footer to the bottom of the body
 document.body.appendChild(footer);
-
-
