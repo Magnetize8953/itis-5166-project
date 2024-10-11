daa74b92c7663b07194dd2cf62c91b55bac056ac
Pineapple Farmer <dlopez18@uncc.edu>
2024-10-11 13:48:05 -0400

    fix: changed parameters in event.js for events to match requirements. refactored other pages to fit changes.

--- models/event.js
+++ models/event.js
@@ -4,63 +4,63 @@ const { v4: uuidv4 } = require('uuid')
 const events = [
   {
     id: '1',
-    organizer: 'UNC Charlotte Career Center',
-    title: 'Career Fair',
     category: 'professional',
-    date: DateTime.fromObject({ year: 2024, month: 9, day: 13, hour: 11, minute: 0 }).toLocaleString(DateTime.DATETIME_MED),
-    location: 'Barnhardt Student Activity Center, Salons',
-    description: 'Meet employers, learn about companies, build your network!',
-    img_src: '/images/Aerial_Clocktower_Atkins.jpg'
+    title: 'Career Fair',
+    hostName: 'UNC Charlotte Career Center',
+    startDateTime: '2024-09-13T11:00',
+    endDateTime: '2024-09-13T15:00',
+    details: 'Meet employers, learn about companies, build your network!',
+    image: '/images/Aerial_Clocktower_Atkins.jpg'
   },
   {
     id: '2',
-    organizer: 'UNC Charlotte Career Center',
-    title: 'Resume Review',
     category: 'professional',
-    date: DateTime.fromObject({ year: 2024, month: 9, day: 4, hour: 14, minute: 30 }).toLocaleString(DateTime.DATETIME_MED),
-    location: 'Atkins Library Annex',
-    description: 'Get your resume reviewed by professionals!',
-    img_src: '/images/Aerial_Atkins_Friday.jpg'
+    title: 'Resume Review',
+    hostName: 'UNC Charlotte Career Center',
+    startDateTime: '2024-09-04T14:30',
+    endDateTime: '2024-09-04T16:30',
+    details: 'Get your resume reviewed by professionals!',
+    image: '/images/Aerial_Atkins_Friday.jpg'
   },
   {
     id: '3',
-    organizer: 'ColorStack at UNC Charlotte',
-    title: 'Game Night',
     category: 'social',
-    date: DateTime.fromObject({ year: 2024, month: 9, day: 30, hour: 19, minute: 0 }).toLocaleString(DateTime.DATETIME_MED),
-    location: 'Woodward Hall, Room 154',
-    description: 'Relax for a night and play some games with fellow ColorStackers!',
-    img_src: '/images/Woodward_from_Hauser_Alumni_Pavilion.jpg'
+    title: 'Game Night',
+    hostName: 'ColorStack at UNC Charlotte',
+    startDateTime: '2024-09-30T19:00',
+    endDateTime: '2024-09-30T22:00',
+    details: 'Relax for a night and play some games with fellow ColorStackers!',
+    image: '/images/Woodward_from_Hauser_Alumni_Pavilion.jpg'
   },
   {
     id: '4',
-    organizer: 'ColorStack at UNC Charlotte',
-    title: 'Social Meetup',
     category: 'social',
-    date: DateTime.fromObject({ year: 2024, month: 9, day: 16, hour: 16, minute: 30 }).toLocaleString(DateTime.DATETIME_MED),
-    location: 'Star Quad',
-    description: 'Meet other members of ColorStack and socialize!',
-    img_src: '/images/Aerial_CHHS_Atkins_Clocktower.jpg'
+    title: 'Social Meetup',
+    hostName: 'ColorStack at UNC Charlotte',
+    startDateTime: '2024-09-16T16:30',
+    endDateTime: '2024-09-16T18:30',
+    details: 'Meet other members of ColorStack and socialize!',
+    image: '/images/Aerial_CHHS_Atkins_Clocktower.jpg'
   },
   {
     id: '5',
-    organizer: 'ColorStack at UNC Charlotte',
-    title: 'Interests Exploration',
     category: 'social',
-    date: DateTime.fromObject({ year: 2024, month: 9, day: 23, hour: 16, minute: 30 }).toLocaleString(DateTime.DATETIME_MED),
-    location: 'Woodward Hall, Room 106',
-    description: 'Share your interests with your colleagues, learn about theirs. Maybe you\'ll find something new to try out!',
-    img_src: '/images/Aerial_Woodward_Campus.jpg'
+    title: 'Interests Exploration',
+    hostName: 'ColorStack at UNC Charlotte',
+    startDateTime: '2024-09-23T16:30',
+    endDateTime: '2024-09-23T18:30',
+    details: 'Share your interests with your colleagues, learn about theirs. Maybe you\'ll find something new to try out!',
+    image: '/images/Aerial_Woodward_Campus.jpg'
   },
   {
     id: '6',
-    organizer: 'ColorStack at UNC Charlotte',
-    title: 'Lunch and Learn',
     category: 'professional',
-    date: DateTime.fromObject({ year: 2024, month: 9, day: 25, hour: 13, minute: 0 }).toLocaleString(DateTime.DATETIME_MED),
-    location: 'Cone University Center, Lucas Room',
-    description: 'Enjoy a free lunch while listening to presentations by fellow students and prospective employers!',
-    img_src: '/images/Aerial_Atkins_Clocktower.jpg'
+    title: 'Lunch and Learn',
+    hostName: 'ColorStack at UNC Charlotte',
+    startDateTime: '2024-09-25T13:00',
+    endDateTime: '2024-09-25T14:30',
+    details: 'Enjoy a free lunch while listening to presentations by fellow students and prospective employers!',
+    image: '/images/Aerial_Atkins_Clocktower.jpg'
   }
 ]
 
@@ -71,26 +71,24 @@ exports.findById = (id) => { return events.find(event => event.id == id) }
 exports.save = (event) => {
   event.id = uuidv4()
   event.category = event.category.charAt(0).toLowerCase() + event.category.slice(1)
-  event.date = DateTime.fromObject({
-    year: parseInt(event.when.slice(0, 4)), month: parseInt(event.when.slice(5, 7)), day: parseInt(event.when.slice(8)),
-    hour: parseInt(event.start.slice(0, 3)), minute: parseInt(event.start.slice(4))
-  }).toLocaleString(DateTime.DATETIME_MED)
+  event.startDateTime = `${event.when}T${event.start}`
+  event.endDateTime = `${event.when}T${event.end}`
+  delete event.when
+  delete event.start
+  delete event.end
   events.push(event)
 }
 
 exports.update = (id, newEvent) => {
   let event = events.find(event => event.id == id)
   if (event) {
-    event.organizer = newEvent.organizer
+    event.hostName = newEvent.hostName
     event.title = newEvent.title
     event.category = newEvent.category.charAt(0).toLowerCase() + newEvent.category.slice(1)
-    event.date = DateTime.fromObject({
-      year: parseInt(newEvent.when.slice(0, 4)), month: parseInt(newEvent.when.slice(5, 7)), day: parseInt(newEvent.when.slice(8)),
-      hour: parseInt(newEvent.start.slice(0, 3)), minute: parseInt(newEvent.start.slice(4))
-    }).toLocaleString(DateTime.DATETIME_MED)
-    event.location = newEvent.location
-    event.description = newEvent.description
-    event.img_src = newEvent.img_src
+    event.startDateTime = `${newEvent.when}T${newEvent.start}`
+    event.endDateTime = `${newEvent.when}T${newEvent.end}`
+    event.details = newEvent.details
+    event.image = newEvent.image
     return true
   }
   return false
