c841c58a20e7c5ff580795954be9ded9d8725383
Magnetize8953 <aha6@charlotte.edu>
2024-10-31 21:24:55 -0400

    feat!: convert to mongodb
    time/date fields have not been converted to date type from string
    category field is not being validated
    
    BREAKING CHANGE: all models, data moved to mongodb database instead of
    being hardcoded in models/event.js

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,78 +1,156 @@
 // require modules
-const { DateTime } = require('luxon')
 const model = require('../models/event')
 
 
 // GET /events: send all events
-exports.index = ('/events', (req, res) => {
-    let events = model.find();
-    let categories = model.getDistinctCategories();
-    res.render('./event/index', { events, categories });
-})
+exports.index = (req, res, next) => {
+    model.find()
+        .then(events => {
+            let categories = [...new Set(events.map(event => event.category))]
+            res.render('./event/index', { events, categories });
+        })
+        .catch(err => { next(err) })
+}
 
 // GET /events/create: send html form for creating new event
-exports.new = ('/events/new', (req, res) => {
+exports.new = (req, res) => {
     res.render('./event/newEvent')
-})
+}
 
 // POST /events: create new event
-exports.create = ('/', (req, res, next) => {
-    let event = req.body
-    event.image = '/images/' + req.file.filename
-    model.save(event)
-    res.redirect('/events')
-})
+exports.create = (req, res, next) => {
+
+    // fix up req.body
+    req.body.category = req.body.category.charAt(0).toLowerCase() + req.body.category.slice(1)
+    req.body.startDateTime = `${req.body.when}T${req.body.start}`
+    req.body.endDateTime = `${req.body.when}T${req.body.end}`
+    req.body.image = '/images/' + req.file.filename
+    delete req.body.when
+    delete req.body.start
+    delete req.body.end
+
+    let event = new model(req.body)
+
+    event.save()
+        .then((event) => {
+            res.redirect('/events')
+        })
+        .catch(err => {
+            if (err.name === 'ValidationError') {
+                err.status = 400
+            }
+            next(err)
+        })
+}
 
 // GET /events/:id: send details about event id
-exports.show = ('/:id', (req, res, next) => {
+exports.show = (req, res, next) => {
     let id = req.params.id
-    let event = model.findById(id)
-    if (event) {
-        res.render('./event/eventDetail', { event })
-    } else {
-        let err = new Error(`Cannot find event with id ${id}`)
-        err.status = 404
-        next(err)
+
+    // id needs to be a 24-bit hex string
+    if (!id.match(/^[0-9a-fA-F]{24}$/)) {
+        let err = new Error('Invalid event id')
+        err.status = 400
+        return next(err)
     }
-})
+
+    model.findById(id)
+        .then(event => {
+            if (event) {
+                res.render('./event/eventDetail', { event })
+            } else {
+                let err = new Error(`Cannot find a event with id ${id}`)
+                err.status = 404
+                next(err)
+            }
+        })
+        .catch(err => next(err))
+}
 
 // GET /events/:id/edit: send html form for editing existing event
-exports.edit = ('/:id/edit', (req, res, next) => {
+exports.edit = (req, res, next) => {
     let id = req.params.id
-    let event = model.findById(id)
-    if (event) {
-        res.render('./event/editEvent', { event })
-    } else {
-        let err = new Error(`Cannot find event with id ${id}`)
-        err.status = 404
-        next(err)
+
+    // id needs to be a 24-bit hex string
+    if (!id.match(/^[0-9a-fA-F]{24}$/)) {
+        let err = new Error('Invalid event id')
+        err.status = 400
+        return next(err)
     }
-})
+
+    model.findById(id)
+        .then(event => {
+            if (event) {
+                res.render('./event/editEvent', { event })
+            } else {
+                let err = new Error(`Cannot find a event with id ${id}`)
+                err.status = 404
+                next(err)
+            }
+        })
+        .catch(err => next(err))
+}
 
 // PUT /events/:id: update event id
-exports.update = ('/:id', (req, res, next) => {
+exports.update = (req, res, next) => {
     let event = req.body
     let id = req.params.id
     let img = req.file.filename
 
-    if (model.update(id, event, img)) {
-        res.redirect(`/events/${id}`)
-    } else {
-        let err = new Error(`Cannot find event with id ${id}`)
-        err.status = 404
-        next(err)
+    // fix up event
+    event.category = event.category.charAt(0).toLowerCase() + event.category.slice(1)
+    event.startDateTime = `${event.when}T${event.start}`
+    event.endDateTime = `${event.when}T${event.end}`
+    event.image = '/images/' + img
+    delete event.when
+    delete event.start
+    delete event.end
+
+    // id needs to be a 24-bit hex string
+    if (!id.match(/^[0-9a-fA-F]{24}$/)) {
+        let err = new Error('Invalid event id')
+        err.status = 400
+        return next(err)
     }
-})
+
+    model.findByIdAndUpdate(id, event, { useFindAndModify: false, runValidators: true })
+        .then(event => {
+            if (event) {
+                res.redirect(`/events/${id}`)
+            } else {
+                let err = new Error(`Cannot find a event with id ${id}`)
+                err.status = 404
+                next(err)
+            }
+        })
+        .catch(err => {
+            if (err.name === 'ValidationError') {
+                err.status = 400
+            }
+            next(err)
+        })
+}
 
 // DELETE /events/:id: delete event id
-exports.delete = ('/:id', (req, res, next) => {
+exports.delete = (req, res, next) => {
     let id = req.params.id
 
-    if (model.delete(id)) {
-        res.redirect('/events')
-    } else {
-        let err = new Error(`Cannot find event with id ${id}`)
-        err.status = 404
-        next(err)
+    // id needs to be a 24-bit hex string
+    if (!id.match(/^[0-9a-fA-F]{24}$/)) {
+        let err = new Error('Invalid event id')
+        err.status = 400
+        return next(err)
     }
-})
+
+    model.findByIdAndDelete(id, { useFindAndModify: true })
+        .then(event => {
+            if (event) {
+                res.redirect('/events')
+            } else {
+                let err = new Error(`Cannot find event with id ${id}`)
+                err.status = 404
+                next(err)
+            }
+        })
+        .catch(err => next(err))
+}
--- models/event.js
+++ models/event.js
@@ -1,115 +1,18 @@
-const { DateTime } = require('luxon')
-const { v4: uuidv4 } = require('uuid')
+// require modules
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
-const events = [
-  {
-    id: '1',
-    category: 'professional',
-    title: 'Career Fair',
-    organizer: 'UNC Charlotte Career Center',
-    startDateTime: '2024-09-13T11:00',
-    endDateTime: '2024-09-13T15:00',
-    location: 'Barnhardt Student Activity Center, Salons',
-    description: 'Meet employers, learn about companies, build your network!',
-    image: '/images/Aerial_Clocktower_Atkins.jpg'
-  },
-  {
-    id: '2',
-    category: 'professional',
-    title: 'Resume Review',
-    organizer: 'UNC Charlotte Career Center',
-    startDateTime: '2024-09-04T14:30',
-    endDateTime: '2024-09-04T16:30',
-    location: 'Atkins Library Annex',
-    description: 'Get your resume reviewed by professionals!',
-    image: '/images/Aerial_Atkins_Friday.jpg'
-  },
-  {
-    id: '3',
-    category: 'social',
-    title: 'Game Night',
-    organizer: 'ColorStack at UNC Charlotte',
-    startDateTime: '2024-09-30T19:00',
-    endDateTime: '2024-09-30T22:00',
-    location: 'Woodward Hall, Room 154',
-    description: 'Relax for a night and play some games with fellow ColorStackers!',
-    image: '/images/Woodward_from_Hauser_Alumni_Pavilion.jpg'
-  },
-  {
-    id: '4',
-    category: 'social',
-    title: 'Social Meetup',
-    organizer: 'ColorStack at UNC Charlotte',
-    startDateTime: '2024-09-16T16:30',
-    endDateTime: '2024-09-16T18:30',
-    location: 'Star Quad',
-    description: 'Meet other members of ColorStack and socialize!',
-    image: '/images/Aerial_CHHS_Atkins_Clocktower.jpg'
-  },
-  {
-    id: '5',
-    category: 'social',
-    title: 'Interests Exploration',
-    organizer: 'ColorStack at UNC Charlotte',
-    startDateTime: '2024-09-23T16:30',
-    endDateTime: '2024-09-23T18:30',
-    location: 'Woodward Hall, Room 106',
-    description: 'Share your interests with your colleagues, learn about theirs. Maybe you\'ll find something new to try out!',
-    image: '/images/Aerial_Woodward_Campus.jpg'
-  },
-  {
-    id: '6',
-    category: 'professional',
-    title: 'Lunch and Learn',
-    organizer: 'ColorStack at UNC Charlotte',
-    startDateTime: '2024-09-25T13:00',
-    endDateTime: '2024-09-25T14:30',
-    location: 'Cone University Center, Lucas Room',
-    description: 'Enjoy a free lunch while listening to presentations by fellow students and prospective employers!',
-    image: '/images/Aerial_Atkins_Clocktower.jpg'
-  }
-]
+// set up events schema
+const eventSchema = new Schema({
+  title: { type: String, required: [true, 'title is required'] },
+  category: { type: String, required: [true, 'category is required'] },
+  organizer: { type: String, required: [true, 'organizer is required'] },
+  startDateTime: { type: String, required: [true, 'startDateTime is required'] },
+  endDateTime: { type: String, required: [true, 'endDateTime is required'] },
+  location: { type: String, required: [true, 'location is required'] },
+  description: { type: String, required: [true, 'description is required'] },
+  image: { type: String, required: [true, 'image is required'] },
+})
 
-exports.find = () => { return events }
-
-exports.findById = (id) => { return events.find(event => event.id == id) }
-
-exports.save = (event) => {
-  event.id = uuidv4()
-  event.category = event.category.charAt(0).toLowerCase() + event.category.slice(1)
-  event.startDateTime = `${event.when}T${event.start}`
-  event.endDateTime = `${event.when}T${event.end}`
-  delete event.when
-  delete event.start
-  delete event.end
-  events.push(event)
-}
-
-exports.update = (id, newEvent, img) => {
-  let event = events.find(event => event.id == id)
-  if (event) {
-    event.organizer = newEvent.organizer
-    event.title = newEvent.title
-    event.category = newEvent.category.charAt(0).toLowerCase() + newEvent.category.slice(1)
-    event.startDateTime = `${newEvent.when}T${newEvent.start}`
-    event.endDateTime = `${newEvent.when}T${newEvent.end}`
-    event.location = newEvent.location
-    event.description = newEvent.description
-    event.image = '/images/' + img
-    return true
-  }
-  return false
-}
-
-exports.delete = (id) => {
-  let index = events.findIndex((event) => { return event.id == id })
-  if (index !== -1) {
-    events.splice(index, 1)
-    return true
-  }
-  return false
-}
-
-exports.getDistinctCategories = () => {
-  return [...new Set(events.map(event => event.category))];
-};
+// export schema
+module.exports = mongoose.model('events', eventSchema)
